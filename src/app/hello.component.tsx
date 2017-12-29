69d3712c005fdaebbc6370f24c135aceafdfb455
Sean Chatman <xpointsh@gmail.com>
2017-12-29 11:55:33 -0800

    Added a style class object.

--- src/app/hello.component.tsx
+++ src/app/hello.component.tsx
@@ -1,7 +1,11 @@
 import * as React from 'react';
 
 export class HelloComponent extends React.Component {
+  spanStyle = {
+    color: '#FF0000'
+  };
+
   render() {
-    return <div>Hello World! (From TSX)</div>;
+    return <div><span style={this.spanStyle}>Hello World (From TSX)</span></div>;
   }
 }
